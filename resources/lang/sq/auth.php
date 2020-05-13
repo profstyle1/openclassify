f61749ffa3272f781b3eb7b2d9bba1c5835b63d0
profstyle1 <46463523+profstyle1@users.noreply.github.com>
2020-05-13 11:02:46 +0300

    New translations auth.php (Albanian)

--- resources/lang/sq/auth.php
+++ resources/lang/sq/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Këto kredenciale nuk përputhen me të dhënat tona.',
+    'throttle' => 'Shumë përpjekje për hyrje. Ju lutemi provoni përsëri në: sekonda sekonda.',
 
 ];
